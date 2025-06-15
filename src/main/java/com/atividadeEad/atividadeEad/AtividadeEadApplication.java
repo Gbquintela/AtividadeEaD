7a5f5af077f4681fc70b012a8a0806001d4ead0e
Gabriel <gabriel_martinhoq@hotmail.com>
2025-06-15 10:50:52 -0300

    created packagens and files

---
+++ src/main/java/com/atividadeEad/atividadeEad/AtividadeEadApplication.java
@@ -0,0 +1,13 @@
+package com.atividadeEad.atividadeEad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeEadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeEadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atividadeEad/atividadeEad/AtividadeEadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atividadeEad.atividadeEad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeEadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
